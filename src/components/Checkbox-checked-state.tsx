04e715f5d02d8ba3a551897b070366ed0ac91e4c
omarsanad100 <omarsanad.me@gmail.com>
2026-04-22 22:26:31 +0200

    Add CheckboxCheckedState component for visual representation of checked state

---
+++ src/components/Checkbox-checked-state.tsx
@@ -0,0 +1,25 @@
+export const CheckboxCheckedState = () => {
+  return (
+    <div
+      className='relative flex items-center justify-center w-[23px] h-[23px] rounded-[6px] bg-[#2469F6] border-2 border-[#2469F6] transition-all
+      group-hover:bg-[#5087F8] group-hover:border-[#5087F8]
+      group-data-[pressed=true]:ring-2 group-data-[pressed=true]:ring-[#DBEAFE]
+      group-data-[pressed=true]:bg-blue-700 group-data-[pressed=true]:border-blue-700'
+    >
+      <svg
+        className='w-full h-full text-white'
+        viewBox='0 0 16 16'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path
+          d='M13.3334 4L6.00002 11.3333L2.66669 8'
+          stroke='currentColor'
+          strokeWidth='1'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        />
+      </svg>
+    </div>
+  )
+}
