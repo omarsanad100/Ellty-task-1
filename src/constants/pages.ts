d82f2e04153b74049a1d6a56f447e7db9b04b3fb
omarsanad100 <omarsanad.me@gmail.com>
2026-04-22 22:25:27 +0200

    Add pages constants and type definition for page management

---
+++ src/constants/pages.ts
@@ -0,0 +1,13 @@
+export type Page = {
+  id: number
+  name: string
+}
+
+export const PAGES: Page[] = [
+  { id: 1, name: 'Page 1' },
+  { id: 2, name: 'Page 2' },
+  { id: 3, name: 'Page 3' },
+  { id: 4, name: 'Page 4' },
+  { id: 5, name: 'Page 5' },
+  { id: 6, name: 'Page 6' }
+]
